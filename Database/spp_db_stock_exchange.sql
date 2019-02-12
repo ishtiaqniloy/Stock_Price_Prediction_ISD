d4af31704b949ca185c5ea2980722dbfa2b2ac81
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-02-13 00:45:41 +0600

    database data inserted

---
+++ Database/spp_db_stock_exchange.sql
@@ -0,0 +1,3 @@
+INSERT INTO spp_db.`stock exchange` (St_Code, Name, `Last Updated Index`) VALUES ('ABSJA', 'China', 4389);
+INSERT INTO spp_db.`stock exchange` (St_Code, Name, `Last Updated Index`) VALUES ('NASDAQ', 'Portugal', 3193);
+INSERT INTO spp_db.`stock exchange` (St_Code, Name, `Last Updated Index`) VALUES ('NYSE', 'Russia', 3928);
